ee690c6c4c463c29d61fe006daef2db3d695a7c6
Shane Auckland <shane.auckland@gmail.com>
2015-01-14 11:02:31 +0000

    Add metadata

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             "tungsten"
+maintainer       "VMWare"
+license          "Apache"
+description      "Installs and manages Tungsten replicator, manager and connector"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
