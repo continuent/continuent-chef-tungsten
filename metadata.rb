8f8d1006f03faeb4965ef8950ef400319b5ffef2
Kieren Evans <kevans@inviqa.com>
2015-01-23 12:01:28 +0000

    Fix quoted escaped passwords creating different passwords than intended.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "VMWare"
 license          "Apache"
 description      "Installs and manages Tungsten replicator, manager and connector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 
 depends "selinux", "~> 0.8.0"
