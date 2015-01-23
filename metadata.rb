7892693b527c06c251cc2c58290adaaeb0736102
Kieren Evans <kevans@inviqa.com>
2015-01-23 09:18:53 +0000

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "VMWare"
 license          "Apache"
 description      "Installs and manages Tungsten replicator, manager and connector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          "0.1.4"
 
 depends "selinux", "~> 0.8.0"
