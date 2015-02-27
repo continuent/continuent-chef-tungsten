921a178e2ef7e9027b571ee4fae2341e63742dff
Shane Auckland <shane.auckland@gmail.com>
2015-02-27 21:46:26 +0000

    Add recipe to create tungsten service

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "VMWare"
 license          "Apache"
 description      "Installs and manages Tungsten replicator, manager and connector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.6"
+version          "1.0.0"
 
 depends "selinux", "~> 0.8.0"
